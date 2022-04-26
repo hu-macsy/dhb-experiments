827e55119c4846e3501f41c609df5fc74b774922
vivid <2944106+c-bebop@users.noreply.github.com>
2022-04-26 11:24:34 +0200

    Replace terrace usage of cppexp with gdsb.

--- develop/terrace_measurements/main.cpp
+++ develop/terrace_measurements/main.cpp
@@ -2,11 +2,11 @@
 
 #include <dhb/graph.h>
 
-#include <cppexp/batcher.h>
-#include <cppexp/experiment.h>
-#include <cppexp/graph.h>
-#include <cppexp/graph_io.h>
-#include <cppexp/timer.h>
+#include <gdsb/batcher.h>
+#include <gdsb/experiment.h>
+#include <gdsb/graph.h>
+#include <gdsb/graph_io.h>
+#include <gdsb/timer.h>
 
 // clang-format off
 #include <graph.h>
@@ -60,7 +60,7 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    cppexp::out("threads", omp_get_max_threads());
+    gdsb::out("threads", omp_get_max_threads());
     if (desired_thread_count && omp_get_max_threads() != desired_thread_count)
         throw std::runtime_error("number of workers does not match desired thread count");
     bool is_parallel = omp_get_max_threads() > 1;
@@ -69,21 +69,20 @@ int main(int argc, char** argv) {
     dhb::Edges edges;
     std::uniform_real_distribution<float> distrib{0.0f, 1.0f};
     std::ifstream graph_input(graph_path);
-    cppexp::TimestampedEdges<dhb::Edges> timestamped_edges;
+    gdsb::TimestampedEdges<dhb::Edges> timestamped_edges;
 
     size_t edges_read_in = 0;
     size_t as_nv = 0;
     if (!temporal_graph_file) {
-        cppexp::read_graph_unweighted<dhb::Vertex>(graph_input,
-                                                   [&](unsigned int u, unsigned int v) {
-                                                       auto w = distrib(prng);
-                                                       // Generate a random edge weight in [a, b).
-                                                       edges.push_back({u, {v, w, 0u}});
-                                                   });
+        gdsb::read_graph_unweighted<dhb::Vertex>(graph_input, [&](unsigned int u, unsigned int v) {
+            auto w = distrib(prng);
+            // Generate a random edge weight in [a, b).
+            edges.push_back({u, {v, w, 0u}});
+        });
         edges_read_in = edges.size();
         as_nv = dhb::graph::vertex_count(edges);
     } else {
-        cppexp::read_temporal_graph<dhb::Vertex>(
+        gdsb::read_temporal_graph<dhb::Vertex>(
             graph_input, weighted_graph_file, [&](unsigned int u, unsigned int v, unsigned int t) {
                 auto w = distrib(prng);
                 // Generate a random edge weight in [a, b).
@@ -91,7 +90,7 @@ int main(int argc, char** argv) {
                 timestamped_edges.timestamps.push_back(t);
             });
 
-        timestamped_edges = cppexp::sort(timestamped_edges);
+        timestamped_edges = gdsb::sort(timestamped_edges);
         edges = timestamped_edges.edges;
     }
 
@@ -100,11 +99,11 @@ int main(int argc, char** argv) {
 
     std::chrono::milliseconds duration;
 
-    unsigned long long memory_footprint_kb_before_construction = cppexp::memory_usage_in_kb();
+    unsigned long long memory_footprint_kb_before_construction = gdsb::memory_usage_in_kb();
     auto before_ctor = std::chrono::high_resolution_clock::now();
     graphstore::Graph graph(as_nv);
     auto after_ctor = std::chrono::high_resolution_clock::now();
-    unsigned long long memory_footprint_kb_after_construction = cppexp::memory_usage_in_kb();
+    unsigned long long memory_footprint_kb_after_construction = gdsb::memory_usage_in_kb();
 
     size_t vertex_count_before = graph.get_num_vertices();
     size_t edge_count_before = graph.get_num_edges();
@@ -159,9 +158,9 @@ int main(int argc, char** argv) {
     auto do_insert_batch = [&]() {
         if (!batch_size)
             throw std::runtime_error("batch size required");
-        cppexp::Batcher<dhb::Edges> batcher(std::begin(edges), std::end(edges), batch_size);
+        gdsb::Batcher<dhb::Edges> batcher(std::begin(edges), std::end(edges), batch_size);
 
-        cppexp::Batch<dhb::Edges> batch = batcher.next_sorted(comp, 1);
+        gdsb::Batch<dhb::Edges> batch = batcher.next_sorted(comp, 1);
         std::vector<uint32_t> src(batch_size);
         std::vector<uint32_t> dest(batch_size);
         std::vector<uint32_t> weight(batch_size);
@@ -188,17 +187,17 @@ int main(int argc, char** argv) {
     };
 
     if (insertion_routine == "insert") {
-        duration = cppexp::benchmark(do_insert);
+        duration = gdsb::benchmark(do_insert);
     } else if (insertion_routine == "insert_bulk") {
-        duration = cppexp::benchmark(do_insert_bulk);
+        duration = gdsb::benchmark(do_insert_bulk);
     } else if (insertion_routine == "insert_batch") {
-        duration = cppexp::benchmark(do_insert_batch);
+        duration = gdsb::benchmark(do_insert_batch);
     } else if (insertion_routine == "temporal_insertions") {
         // Temporal insertions = plain insertions (insertion still checks for edge existance).
-        duration = cppexp::benchmark(do_insert);
+        duration = gdsb::benchmark(do_insert);
     } else if (insertion_routine == "temporal_deletions") {
         auto collected = collect_random_edges();
-        duration = cppexp::benchmark(do_temporal_deletions, collected);
+        duration = gdsb::benchmark(do_temporal_deletions, collected);
     } else if (insertion_routine == "bfs") {
         size_t num_iters = 100;
         size_t num_inserts = 100000;
@@ -211,7 +210,7 @@ int main(int argc, char** argv) {
             ++off;
         }
 
-        duration = cppexp::benchmark([&] {
+        duration = gdsb::benchmark([&] {
             for (size_t l = 0; l < 100; ++l) {
                 for (size_t i = 0; i < num_inserts; ++i) {
                     if (off >= edges.size())
@@ -250,7 +249,7 @@ int main(int argc, char** argv) {
                 f(col_inds[p], vals[p]);
         };
 
-        duration = cppexp::benchmark([&] {
+        duration = gdsb::benchmark([&] {
             size_t nnz = 0; // Stop after a number of non-zeros.
             std::vector<dhb::Vertex> marker(as_nv);
             for (dhb::Vertex i = 0; i < as_nv; ++i) {
@@ -278,28 +277,28 @@ int main(int argc, char** argv) {
     size_t vertex_count_after = graph.get_num_vertices();
     size_t edge_count_after = graph.get_num_edges();
 
-    unsigned long long memory_footprint_kb_after_insertion = cppexp::memory_usage_in_kb();
+    unsigned long long memory_footprint_kb_after_insertion = gdsb::memory_usage_in_kb();
 
-    cppexp::out("experiment", experiment_name);
-    cppexp::out("graph", graph_path.filename());
-    cppexp::out("edges_read_in", edges_read_in);
-    cppexp::out("batch_size", batch_size);
+    gdsb::out("experiment", experiment_name);
+    gdsb::out("graph", graph_path.filename());
+    gdsb::out("edges_read_in", edges_read_in);
+    gdsb::out("batch_size", batch_size);
     bool temporal_graph = false;
-    cppexp::out("temporal_graph", temporal_graph);
-    cppexp::out("insertion_routine", insertion_routine);
+    gdsb::out("temporal_graph", temporal_graph);
+    gdsb::out("insertion_routine", insertion_routine);
     unsigned int insert_factor = 15;
-    cppexp::out("insert_factor", insert_factor);
-    cppexp::out("format", "terrace");
-    cppexp::out("vertex_count_before", vertex_count_before);
-    cppexp::out("edge_count_before", edge_count_before);
-    cppexp::out("edge_count_after", edge_count_after);
-    cppexp::out("duration_ms", duration.count());
-    cppexp::out("memory_footprint_kb_before_construction", memory_footprint_kb_before_construction);
-    cppexp::out("memory_footprint_kb_after_construction", memory_footprint_kb_after_construction);
-    cppexp::out("memory_footprint_kb_after_insertion", memory_footprint_kb_after_insertion);
+    gdsb::out("insert_factor", insert_factor);
+    gdsb::out("format", "terrace");
+    gdsb::out("vertex_count_before", vertex_count_before);
+    gdsb::out("edge_count_before", edge_count_before);
+    gdsb::out("edge_count_after", edge_count_after);
+    gdsb::out("duration_ms", duration.count());
+    gdsb::out("memory_footprint_kb_before_construction", memory_footprint_kb_before_construction);
+    gdsb::out("memory_footprint_kb_after_construction", memory_footprint_kb_after_construction);
+    gdsb::out("memory_footprint_kb_after_insertion", memory_footprint_kb_after_insertion);
     bool update = false;
-    cppexp::out("update", update);
-    cppexp::out("vertex_count_after", vertex_count_after);
+    gdsb::out("update", update);
+    gdsb::out("vertex_count_after", vertex_count_after);
 
     return 0;
 }
